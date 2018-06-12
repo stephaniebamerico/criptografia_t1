0199b92e567098630ea2eaee82a516797990869b
Talita <thcf16@inf.ufpr.br>
2018-06-12 17:44:47 -0300

    mudei umas coisas pra ele botar um arquivo random junto do criptografado pra saida ficar maior e ter algo pra ouvir no audio
    Signed-off-by: Talita <thcf16@inf.ufpr.br>

--- escreveBinario.c
+++ escreveBinario.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-#define SZ 50000
+#include <stdlib.h>
+#include <time.h>
+
+//#define SZ 5000
 int main(int argc, char const *argv[])
 {
+
+
+    time_t t;
+    srand((unsigned) time(&t));
+ 
+    int SZ;
+    printf("Tamanho da entrada:\n");
+    scanf ("%d", &SZ);
     FILE *ptr;
     ptr = fopen("test.bin", "wb");
     unsigned char buffer[SZ];
     for (int i = 0; i < SZ; ++i)
     {
-        buffer[i]=i;
+        buffer[i]=(char)rand();
     }
     fwrite(buffer, SZ, 1, ptr);
-    fclose(ptr);
-    /*ptr = fopen("waveHeader.bin", "rb");
-    fread(buffer, 128, 1, ptr);
-    for (int i = 0; i < 128; ++i)
-    {
-        printf("%3d %c\n", buffer[i],buffer[i]);
-    }*/
     return 0;
 }
