d4a9eb11315ebb900acbeb1847d496bf225c01a7
Stephanie Briere Americo <sba16@c3sl.ufpr.br>
2018-06-15 09:23:59 -0300

    Vigenere expandido

--- vigenere_autokey.py
+++ vigenere_autokey.py
@@ -1,40 +1,40 @@
+import sys
+
 def encrypt(key_s, message_s):
+	alphabet_init=30
+	alphabet_size=226
+
 	#Change it to array to use useful methods
-	key_s=key_s.upper()
 	key=[]
 	for e in key_s:
 		key.append(e)
 
-	message_s=message_s.upper()
 	message=[]
 	for e in message_s:
 		message.append(e)
-
-	#Remove spaces from message
-	for unused in range(len(message)):
-		if " " in message:
-			message.remove(" ")
 	
 	cipher=[]
 	i=0
 	for i in range(len(message)):
 		key.append(message[i])
 
-		p = ord(message[i]) - 65
-		k = ord(key[i]) - 65
-		c = (p+k)%26 + 65
+		p = ord(message[i]) - alphabet_init
+		k = ord(key[i]) - alphabet_init
+		c = (p+k)%alphabet_size + alphabet_init
 		cipher.append(chr(c))
 
 	return ''.join(cipher)
 
 def decrypt(key_s, cipher_s):
+	alphabet_init=30
+	alphabet_size=226
+
 	#Change it to array to use useful methods
-	key_s=key_s.upper()
+	key_s=key_s
 	key=[]
 	for e in key_s:
 		key.append(e)
 
-	cipher_s=cipher_s.upper()
 	cipher=[]
 	for e in cipher_s:
 		cipher.append(e)
@@ -42,18 +42,32 @@ def decrypt(key_s, cipher_s):
 	message=[]
 	i=0
 	for i in range(len(cipher)):
-		c = ord(cipher[i]) - 65
-		k = ord(key[i]) - 65
-		p = (c-k+26)%26 + 65
+		c = ord(cipher[i]) - alphabet_init
+		k = ord(key[i]) - alphabet_init
+		p = (c-k+alphabet_size)%alphabet_size + alphabet_init
 		message.append(chr(p))
 
 		key.append(message[i])
 
 	return ''.join(message)
 
-###
+##main
+order=input()
+if order==1:
+	key=raw_input()
+	with open("claro", "r") as f:
+	    message=f.read()
+	
+	message=encrypt(key, message)
 
-k=raw_input("Chave: ")
-m=raw_input("Msg: ")
+	sys.stdout.write(message)
+elif order==2:
+	key=raw_input()
+	with open("criptografado", "rb") as f:
+	    cipher=f.read()
 
-print decrypt(k, m)
+	cipher=decrypt(key, cipher)
+
+	sys.stdout.write(cipher)
+else:
+	print "Error"
